76c986dcb9743250e67cdb17e251c1b6aef51696
Long Do <me@dotronglong.com>
2016-08-15 00:25:10 +0700

    Update api document for Str

--- dist/str.js
+++ dist/str.js
@@ -8,23 +8,26 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+/**
+ * String processor
+ * @todo Manipulate string
+ */
 var Str = function () {
   function Str() {
     _classCallCheck(this, Str);
   }
 
   _createClass(Str, null, [{
-    key: 'lcfirst',
-    value: function lcfirst(string) {
-      return string.charAt(0).toLowerCase() + string.slice(1);
-    }
-  }, {
-    key: 'ucfirst',
-    value: function ucfirst(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-  }, {
     key: 'changeFirstCase',
+
+    /**
+     * Modify first case of string with delimiter
+     * @access private
+     * @param {!string} string String to modify first case
+     * @param {?string} delimiter Delimiter of string
+     * @param {?function} callback Callback to deal with character
+     * @returns {string}
+     */
     value: function changeFirstCase(string, delimiter, callback) {
       var pattern = new RegExp('^([a-zA-Z])|\\' + delimiter + '([a-zA-Z])', 'ig');
       string.replace(pattern, function () {
@@ -38,19 +41,59 @@ var Str = function () {
       });
       return string;
     }
+
+    /**
+     * Change the first case of string (or before delimiter character) to upper case
+     * @param {!string} string String to modify first case
+     * @param {?string} delimiter Delimiter of string
+     * @returns {string}
+     */
+
   }, {
     key: 'upperCaseFirst',
     value: function upperCaseFirst(string, delimiter) {
       return this.changeFirstCase(string, delimiter, function (char) {
-        return Str.ucfirst(char);
+        return char.toUpperCase();
       });
     }
+
+    /**
+     * Change the first case of string (or before delimiter character) to lower case
+     * @param {!string} string String to modify first case
+     * @param {?string} delimiter Delimiter of string
+     * @returns {string}
+     */
+
   }, {
     key: 'lowerCaseFirst',
     value: function lowerCaseFirst(string, delimiter) {
       return this.changeFirstCase(string, delimiter, function (char) {
-        return Str.lcfirst(char);
+        return char.toLowerCase();
       });
+    }
+
+    /**
+     * Change the first case of string to lower case
+     * @param {!string} string String to modify first case
+     * @returns {string}
+     */
+
+  }, {
+    key: 'lcfirst',
+    value: function lcfirst(string) {
+      return Str.lowerCaseFirst(string);
+    }
+
+    /**
+     * Change the first case of string to upper case
+     * @param {!string} string String to modify first case
+     * @returns {string}
+     */
+
+  }, {
+    key: 'ucfirst',
+    value: function ucfirst(string) {
+      return Str.upperCaseFirst(string);
     }
   }]);
 
